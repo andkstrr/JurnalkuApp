4aa6ff4f9b32f277d9ceeb3873ed36c9905dbff7
andkstrr <andikasatrionurcahyo@gmail.com>
2025-11-17 11:20:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:jurnalku_app/screens/splash.dart';
+
+void main() => runApp(MaterialApp(
+  home: JurnalkuApp(),
+));
+
+class JurnalkuApp extends StatelessWidget {
+  const JurnalkuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/screens/splash.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:jurnalku_app/screens/settings.dart';
+// Splash Screen Utama
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();  
+    // Future.delayed akan menjalankan fungsi callback setelah durasi yang ditentukan (3 detik)
+    Future.delayed(const Duration(seconds: 3), () {
+      // pushReplacement mencegah pengguna kembali ke splash screen dengan tombol kembali
+      if (mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const Settings()),
+        );
+      }
+    });
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0EB34C),
+      body: Center(
+        child: SizedBox(
+          width: 230,
+          height: 230,
+          child: Image(
+            image: AssetImage('assets/logo/furlo.png'),
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}
