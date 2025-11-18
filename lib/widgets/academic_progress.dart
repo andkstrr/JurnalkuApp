8056caded71dfb2435db892b285cd3e675220342
andkstrr <andikasatrionurcahyo@gmail.com>
2025-11-18 07:53:04 +0700

    add dashboard page

--- lib/widgets/academic_progress.dart
+++ lib/widgets/academic_progress.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:jurnalku_app/main.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AcademicProgress extends StatelessWidget {
